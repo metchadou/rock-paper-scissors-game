2050955d629e7db4b16cbff470d2ae8e0838c1a9
momotorres <adoufranklin94@gmail.com>
2026-02-11 07:51:18 +0000

    Add UI

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const ROUNDS = 5;
 let humanScore = 0;
 let computerScore = 0;
+let roundCount = 0;
+const body = document.querySelector("body");
+const scoreBoard = document.createElement("div");
+scoreBoard.textContent = `Human score: ${humanScore}\nComputer score: ${computerScore}`;
+scoreBoard.classList.add("temp");
 
 //Logic to get the computer choice
 
@@ -14,67 +18,141 @@ function getRandomIndex(arrayLength) {
     return Math.floor(Math.random() * arrayLength);
 }
 
-// console.log(getComputerChoice());
-
-//Logic to get the human choice
-function getHumanChoice() {
-    const userInput = prompt('Choose rock, paper or scissors\nType:\nr for rock\np for paper\ns for scissors');
-    switch (userInput) {
-        case 'r':
-            return 'rock';
-            break;
-        case 'p':
-            return 'paper';
-        case 's':
-            return 'scissors';
-        default:
-            return '';
-    }
-}
-
-//console.log(getHumanChoice());
-
 //Logic to play a round
 
 function playRound(humanChoice, computerChoice) {
-    const humanSelection = humanChoice.toLowerCase();
-    const computerSelection = computerChoice.toLowerCase();
+    const div = document.createElement("div");
+    div.classList.add("clear");
 
-    if ((humanSelection == "rock" && computerSelection == "paper")
-        || (humanSelection == "paper" && computerSelection == "scissors")
-        || (humanSelection == "scissors" && computerSelection == "rock")
+    if ((humanChoice == "rock" && computerChoice == "paper")
+        || (humanChoice == "paper" && computerChoice == "scissors")
+        || (humanChoice == "scissors" && computerChoice == "rock")
     ) {
-        console.log(`You lose! ${computerSelection} beats ${humanSelection}`);
+        div.textContent = `Round ${roundCount}: You lose! ${computerChoice} beats ${humanChoice}`;
         computerScore++;
-    } else if (humanSelection == computerSelection) {
-        console.log(`Tie! You both selected ${humanSelection}`);
+    } else if (humanChoice == computerChoice) {
+        div.textContent = `Round ${roundCount}: Tie! You both selected ${humanChoice}`;
     } else {
-        console.log(`You win! ${humanSelection} beats ${computerSelection}`);
+        div.textContent = `Round ${roundCount}: You win! ${humanChoice} beats ${computerChoice}`;
         humanScore++;
     }
+
+    body.appendChild(div);
+}
+
+//Logic to check for a winner
+
+function getWinner() {
+    const winner = humanScore == 5 ? "human" :
+                    computerScore == 5 ? "computer" :
+                     "";
+    return winner;
+}
+
+// Logic to announce game winner
+
+function announceGameWinner(winner) {
+    const div = document.createElement("div");
+    div.classList.add("clear");
+
+    switch (winner) {
+        case "human":
+            div.textContent = `YOU WIN!`;
+            break;
+        case "computer":
+            div.textContent = `YOU LOSE.`;
+            break;
+        default:
+            break;
+    }
+
+    body.appendChild(div);
     
 }
 
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
+// Logic to clear board
 
-// playRound(humanSelection, computerSelection);
+function clearBoard() {
+    document.querySelectorAll(".clear").forEach(el => el.remove());
+}
+
+
+//Logic to reset game
+
+function resetGame() {
+    roundCount = 0;
+    humanScore = 0;
+    computerScore = 0;
+}
+
+// Logic to display score
+
+function displayScore() {
+    const scoreBoard = document.createElement("div");
+    const humanScoreContainer = document.createElement("div");
+    const computerScoreContainer = document.createElement("div");
+
+    humanScoreContainer.textContent = `Your score: ${humanScore}`;
+    computerScoreContainer.textContent = `Computer score: ${computerScore}`;
+
+    scoreBoard.appendChild(humanScoreContainer);
+    scoreBoard.appendChild(computerScoreContainer);
+
+    scoreBoard.classList.add("clear");
+    scoreBoard.style.fontWeight = "900";
+
+    body.appendChild(scoreBoard);
+}
 
 
 //Logic to play a the entire game
 
 function playGame() {
-    for (let i = 0; i < ROUNDS; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
+    const buttonsContainer = document.querySelector('div.buttons-container');
+    const buttons = document.querySelectorAll('div.buttons-container > button');    
 
-    if (humanScore > computerScore) {
-        console.log(`YOU WIN!!!\nYour score: ${humanScore}\nComputer score: ${computerScore}`);
-    } else if (computerScore > humanScore) {
-        console.log(`YOU LOSE.\nYour score: ${humanScore}\nComputer score: ${computerScore}`);
-    } else {
-        console.log(`TIE.\nYour score: ${humanScore}\nComputer score: ${computerScore}`);
-    }
+    buttons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            e.preventDefault();
+            clearBoard();
+            roundCount++;
+            playRound(button.id, getComputerChoice());
+
+            let winner = getWinner();
+
+            if (winner) {
+                buttonsContainer.classList.add("clear");
+                clearBoard();
+                announceGameWinner(winner);
+                displayScore();
+                winner = "";
+
+                const replayButtonContainer = document.createElement("div");
+                const replayButton = document.createElement("button");
+                
+                replayButtonContainer.classList.add("clear");
+                // replayButtonContainer.marginTop = "12px";
+                replayButton.textContent = "Play again";
+
+                replayButtonContainer.appendChild(replayButton);
+                body.appendChild(replayButtonContainer);
+
+                replayButton.addEventListener("click", (e) => {
+                    e.preventDefault();
+                    clearBoard();
+                    resetGame();
+
+                    buttonsContainer.classList.remove("clear");
+
+                    body.appendChild(buttonsContainer);
+                });
+            } else {
+                displayScore();
+            }
+        });
+    });
+
+
 }
 
 playGame();
